85e84594f8c9fad1acece12a0bedbbed60747ce8
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:50:19 +0000

    Update sessionexercise.py

--- PythonModels/sessionexercise.py
+++ PythonModels/sessionexercise.py
@@ -2,9 +2,9 @@ class SessionExercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
    
     session_id = Column(ForeignKey("session.id"))
-    session = relationship("session")
+    session = relationship("Session")
     exercise_id = Column(ForeignKey("exercise.id"))
-    exercise = relationship("exercise")
+    exercise = relationship("Exercise")
 
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
