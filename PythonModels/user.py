1c94952aac0c3c7ddd8bb59f17ce80554095d053
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:34:33 +0000

    Update user.py

--- PythonModels/user.py
+++ PythonModels/user.py
@@ -2,3 +2,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String)
+    measurements = relationship("Measurement")
+    
