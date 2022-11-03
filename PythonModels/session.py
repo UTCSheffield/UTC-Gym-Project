8b67e2233b233d7b266ee09ffc9c93d9be66ef66
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:36:36 +0000

    Update session.py

--- PythonModels/session.py
+++ PythonModels/session.py
@@ -1,5 +1,8 @@
 class Session(db.Model):
     id = db.Column(db.Integer,nullable=False)
+    
+    user_id = Column(Integer, ForeignKey("user.id"))
+    
     User = sa.Column(db.String, nullable=False)
     location = sa.Column(db.String, nullable=False)
     startDateTime = sa.Column(db.DateTime, nullable=False)
