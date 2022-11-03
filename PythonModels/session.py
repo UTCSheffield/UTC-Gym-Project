6081943e5989cc47d7e4154b918675b45fd008d3
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:11:20 +0000

    Update session.py

--- PythonModels/session.py
+++ PythonModels/session.py
@@ -1,2 +1,6 @@
 class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    User = sa.Column(sa.String, primary_key=True)
+    location = sa.Column(sa.String, primary_key=True)
+    startDateTime = sa.Column(sa.DateTime, primary_key=True)
+    endTime = sa.Column(db.DateTime, primary_key=True)
