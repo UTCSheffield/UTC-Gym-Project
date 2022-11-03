c1be1981d2095e5165ba9f104bd17b1e2a3365ba
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:12:27 +0000

    Update exercise.py

--- PythonModels/exercise.py
+++ PythonModels/exercise.py
@@ -1,5 +1,5 @@
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = sa.Column(sa.Integer, primary_key=True)
-    machine = sa.Column(sa.Integer, primary_key=True)
-    measure = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(db.String, primary_key=True)
+    machine = sa.Column(db.String, primary_key=True)
+    measure = sa.Column(db.String, primary_key=True)
