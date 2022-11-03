409cd7a086fc6e5f80e354fef69942facee791ec
James Caroe <83098125+James46113@users.noreply.github.com>
2022-11-03 12:09:34 +0000

    Update sessionexercise.py

--- PythonModels/sessionexercise.py
+++ PythonModels/sessionexercise.py
@@ -1,2 +1,9 @@
 class SessionExercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    #Session link?
+    #Exercise link?
+    #met lerel pex?
+    start = db.Column(db.DateTime, nullable)
+    end = db.Column(db.DateTime)
+    reps = db.Column(db.Integer)
+    
