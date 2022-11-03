3e34bf53b03f7573dd11cab8def6b61d2964fb96
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:29:43 +0000

    Update measurement.py

--- PythonModels/measurement.py
+++ PythonModels/measurement.py
@@ -1,6 +1,6 @@
 class Measurement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     #USER LINK
-    date = db.Column(db.DateTime) # we don't know what all  the datatypes are
-    height = db.Column(db.Integer)
-    resting-heartrate = db.Column(db.Integer)
+    date = db.Column(db.DateTime, nullable=False) # we don't know what all  the datatypes are
+    height = db.Column(db.Integer, nullable=False)
+    resting-heartrate = db.Column(db.Integer, nullable=False)
