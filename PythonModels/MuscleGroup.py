099e42ff7768ed2d22c3b67130d4ac9cdeb9c468
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:59:13 +0000

    Create MuscleGroup.py

---
+++ PythonModels/MuscleGroup.py
@@ -0,0 +1,7 @@
+class Exercise(db.Model):
+    id = db.Column(db.Integer, nullable=False)
+    
+    machine_id = Column(Integer, ForeignKey("Machine_table.id"))
+    machine = relationship("Machine")
+    
+    name = sa.Column(db.String, nullable=False)
