50598cfb0ee2dfe9f9c3be955beb391186302caa
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2022-11-03 12:43:16 +0000

    Update sessionexercise.py

--- PythonModels/sessionexercise.py
+++ PythonModels/sessionexercise.py
@@ -1,8 +1,9 @@
 class SessionExercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #Session link?
-    #Exercise link?
-    #met lerel pex?
+   
+    session_id = Column(ForeignKey("session.id"))
+    exercise_id = Column(ForeignKey("exercise.id"))
+
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
     reps = db.Column(db.Integer, nullable=False)
